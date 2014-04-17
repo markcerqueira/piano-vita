3c2f366358c9835e2efaa8fd45b52da2045450b7
Josh Wu <battleangel@gmail.com>
2014-04-17 16:45:56 -0700

    zeldaaaa

--- SongLoader.cs
+++ SongLoader.cs
@@ -43,6 +43,53 @@ public class SongLoader {
 		pianoNoteDictionary.Add(ticksForBeat(4.5), new PianoNote(ticksForBeat(4.5), 64));
 		pianoNoteDictionary.Add(ticksForBeat(4.75), new PianoNote(ticksForBeat(4.75), 65));
 		pianoNoteDictionary.Add(ticksForBeat(5), new PianoNote(ticksForBeat(5), 67));
+		pianoNoteDictionary.Add(ticksForBeat(7.5), new PianoNote(ticksForBeat(7.5), 67));
+		pianoNoteDictionary.Add(ticksForBeat(8), new PianoNote(ticksForBeat(8), 67));
+		pianoNoteDictionary.Add(ticksForBeat(8.5), new PianoNote(ticksForBeat(8.5), 68));
+		pianoNoteDictionary.Add(ticksForBeat(8.75), new PianoNote(ticksForBeat(8.75), 70));
+		pianoNoteDictionary.Add(ticksForBeat(9), new PianoNote(ticksForBeat(9), 72));
+		
+		pianoNoteDictionary.Add(ticksForBeat(11.5), new PianoNote(ticksForBeat(12.5), 72));
+		pianoNoteDictionary.Add(ticksForBeat(12), new PianoNote(ticksForBeat(13), 72));
+		pianoNoteDictionary.Add(ticksForBeat(12.5), new PianoNote(ticksForBeat(13.5), 70));
+		pianoNoteDictionary.Add(ticksForBeat(13), new PianoNote(ticksForBeat(14), 68));
+		pianoNoteDictionary.Add(ticksForBeat(13.25), new PianoNote(ticksForBeat(14.25), 70));
+		pianoNoteDictionary.Add(ticksForBeat(13.5), new PianoNote(ticksForBeat(14.5), 68));
+		pianoNoteDictionary.Add(ticksForBeat(13.75), new PianoNote(ticksForBeat(14.75), 67));
+		
+		pianoNoteDictionary.Add(ticksForBeat(15), new PianoNote(ticksForBeat(15), 67));		
+		pianoNoteDictionary.Add(ticksForBeat(15.3), new PianoNote(ticksForBeat(15.3), 67));
+		pianoNoteDictionary.Add(ticksForBeat(15.6), new PianoNote(ticksForBeat(15.6), 67));
+		pianoNoteDictionary.Add(ticksForBeat(16), new PianoNote(ticksForBeat(16), 65));
+		pianoNoteDictionary.Add(ticksForBeat(16.5), new PianoNote(ticksForBeat(16.5), 65));
+		pianoNoteDictionary.Add(ticksForBeat(17), new PianoNote(ticksForBeat(17), 67));
+		pianoNoteDictionary.Add(ticksForBeat(17.25), new PianoNote(ticksForBeat(17.25), 68));
+		
+		pianoNoteDictionary.Add(ticksForBeat(18), new PianoNote(ticksForBeat(18), 67));
+		pianoNoteDictionary.Add(ticksForBeat(18.5), new PianoNote(ticksForBeat(18.5), 65));		
+		pianoNoteDictionary.Add(ticksForBeat(19), new PianoNote(ticksForBeat(19), 63));
+		pianoNoteDictionary.Add(ticksForBeat(19.5), new PianoNote(ticksForBeat(19.5), 63));
+		pianoNoteDictionary.Add(ticksForBeat(19.75), new PianoNote(ticksForBeat(19.75), 65));
+		pianoNoteDictionary.Add(ticksForBeat(20), new PianoNote(ticksForBeat(20), 67));
+		
+		pianoNoteDictionary.Add(ticksForBeat(21), new PianoNote(ticksForBeat(21), 65));
+		pianoNoteDictionary.Add(ticksForBeat(21.5), new PianoNote(ticksForBeat(21.5), 63));
+		pianoNoteDictionary.Add(ticksForBeat(22), new PianoNote(ticksForBeat(22), 62));
+		pianoNoteDictionary.Add(ticksForBeat(22.5), new PianoNote(ticksForBeat(22.5), 62));
+		pianoNoteDictionary.Add(ticksForBeat(22.75), new PianoNote(ticksForBeat(22.75), 64));
+		pianoNoteDictionary.Add(ticksForBeat(23), new PianoNote(ticksForBeat(23), 66));
+		
+		pianoNoteDictionary.Add(ticksForBeat(25), new PianoNote(ticksForBeat(25), 69));
+		pianoNoteDictionary.Add(ticksForBeat(26), new PianoNote(ticksForBeat(26), 67));
+		
+		pianoNoteDictionary.Add(ticksForBeat(26.5), new PianoNote(ticksForBeat(26.5), 55));
+		pianoNoteDictionary.Add(ticksForBeat(26.75), new PianoNote(ticksForBeat(26.75), 55));
+		pianoNoteDictionary.Add(ticksForBeat(27), new PianoNote(ticksForBeat(27), 55));
+		pianoNoteDictionary.Add(ticksForBeat(27.5), new PianoNote(ticksForBeat(27.5), 55));
+		pianoNoteDictionary.Add(ticksForBeat(28), new PianoNote(ticksForBeat(28), 55));
+		pianoNoteDictionary.Add(ticksForBeat(28.5), new PianoNote(ticksForBeat(28.5), 55));
+		pianoNoteDictionary.Add(ticksForBeat(29), new PianoNote(ticksForBeat(29), 55));
+		pianoNoteDictionary.Add(ticksForBeat(29.5), new PianoNote(ticksForBeat(29.5), 55));
 		
 		FinishLoadingSongProcess(pianoNoteDictionary, graphics);
 	}
